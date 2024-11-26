764e12272d82eed262263d35c209645a54f0f3ea
mahyar-naseri <naserymahyar10@gmail.com>
2024-11-27 01:21:09 +0330

    Add files via upload

--- Games/Untitled-1.py
+++ Games/Untitled-1.py
@@ -1,67 +1,31 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title("صفحه اصلی بازی")
-window.geometry("400x300")
 
-message_label = tk.Label(window, text="خوش آمدی!", font=("Arial", 12))
-message_label.pack(pady=20)
+window.title("My_project")
 
-def show_about():
-    help = tk.Toplevel(window)
-    help.title("درباره بازی")
-    help.geometry("300x200")
-    tk.Label(help, text=" یک بازی ساده با دو بخش مختلف .", font=("Arial", 12)).pack(pady=20)
-    tk.Button(help, text="بستن", command=help.destroy).pack(pady=10)
+window_width = 600
+window_height = 400
 
-def show_games():
-    start_button.pack_forget()
-    help_button.pack_forget()
-    exit_button.pack_forget()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
 
-    game1_button.pack(pady=10)
-    game2_button.pack(pady=10)
-    back_button.pack(pady=10)
-    message_label.config(text="لطفا یکی از گزینه ها را انتخاب کنید.")
+center_x = int((screen_width / 2) - (window_width / 2))
+center_y = int((screen_height / 2) - (window_height / 2))
 
-def play_game1():
-    game1_window = tk.Toplevel(window)
-    game1_window.title("بازی اول")
-    game1_window.geometry("300x200")
-    tk.Label(game1_window, text="این پنجره بازی اول است!", font=("Arial", 12)).pack(pady=20)
-    tk.Button(game1_window, text="بستن", command=game1_window.destroy).pack(pady=10)
+window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-def play_game2():
-    game2_window = tk.Toplevel(window)
-    game2_window.title("بازی دوم")
-    game2_window.geometry("300x200")
-    tk.Label(game2_window, text="این پنجره بازی دوم است!", font=("Arial", 12)).pack(pady=20)
-    tk.Button(game2_window, text="بستن", command=game2_window.destroy).pack(pady=10)
+window.resizable(True, False)
 
-def go_back():
-    game1_button.pack_forget()
-    game2_button.pack_forget()
-    back_button.pack_forget()
+window.attributes('-alpha', 0.99)
 
-    start_button.pack(pady=10)
-    help_button.pack(pady=10)
-    exit_button.pack(pady=10)
-    message_label.config(text="خوش آمدی!")
+window.lower()
+window.lift()
 
-def exit_game():
-    window.quit()
+window.iconbitmap('./assets/pythontutorial.ico')
 
+my_message = tk.Label(window, text = "My project")
 
-start_button = tk.Button(window, text="ورود به بازی", command=show_games)
-help_button = tk.Button(window, text="درباره بازی", command=show_about)
-exit_button = tk.Button(window, text="خروج از بازی", command=exit_game)
-
-start_button.pack(pady=10)
-help_button.pack(pady=10)
-exit_button.pack(pady=10)
-
-game1_button = tk.Button(window, text="بازی اول", command=play_game1)
-game2_button = tk.Button(window, text="بازی دوم", command=play_game2)
-back_button = tk.Button(window, text="بازگشت", command=go_back)
+my_message.pack()
 
 window.mainloop()
